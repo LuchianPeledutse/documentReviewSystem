8032d524a74d111ced5306bc27fa77906ce8d4a6
Luchian <luchian-1999@mail.ru>
2026-02-22 05:07:09 +0300

    Added batch implementation of loading chunks

--- data_utils.py
+++ data_utils.py
@@ -8,33 +8,38 @@ import torch.nn as nn
 
 from transformers import AutoModel, AutoTokenizer
 
-
+# Classes
 
 class TextEmbedding:
-    def __init__(self, model_name: str):
-        self.model = AutoModel.from_pretrained(model_name)
+    def __init__(self, model_name: str, device: str = "cpu"):
+        self.device = device
+        self.model = AutoModel.from_pretrained(model_name).to(device).eval()
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
     
-    def embed(self, docs: List, max_length: int = 8192) -> np.ndarray:
+    def embed(self, docs: List, max_length: int = 8192, batch_size: int = 3) -> np.ndarray:
         """
-        Embeds list of documents to vector representations
+        Embeds list of documents to vector representations by yilding embedded batches
 
         Args
         ----
         docs: List
             Documents to be embedded
         
-        Returns
+        Yields
         -------
         array: np.ndarray
-            Numpy array of shape (M, N) representing normalized vector embeddings, where
-            M - number of documents in a list; N - dimension of model vectors 
+            Numpy array of shape (B, E) representing normalized vector embeddings, where
+            B - number of documents in a batch; E - dimension of model vectors 
         """
-        docs_tokens = self.tokenizer(docs, return_tensors='pt',
-                                     padding=True, truncation=True, max_length=max_length)
-        docs_embeddings = self.model(**docs_tokens)[0][:, 0]
-        normalized_embeddings = nn.functional.normalize(docs_embeddings, p=2.0, dim=1).detach().numpy()
-        return normalized_embeddings
+        NUM_DOCS = len(docs)
+        for i_batch in range(NUM_DOCS//batch_size):
+            docs_tokens = self.tokenizer(docs[i_batch*batch_size:(i_batch+1)*batch_size if (i_batch+1)*batch_size <= NUM_DOCS else NUM_DOCS], return_tensors='pt',
+                                         padding=True, truncation=True, max_length=max_length)
+
+            docs_device_tokens = {k: v.to(device=self.device) for k, v in docs_tokens.items()}
+            docs_embeddings = self.model(**docs_device_tokens)[0][:, 0]
+            normalized_embeddings = nn.functional.normalize(docs_embeddings, p=2.0, dim=1).detach().cpu().numpy()
+            yield normalized_embeddings
     
 
 class PdfChunkReader():
@@ -71,5 +76,25 @@ class PdfChunkReader():
         self.pdf_chunks = pdf_chunks
         return pdf_chunks
 
+
+# Functions
+
+def normalize_vectors(vectors_array: np.ndarray) -> np.ndarray:
+    """
+    Function that takes in numpy vectors as rows, normalizes and returns them back
+
+    Args
+    ----
+    vectors_array: np.ndarray
+        Numpy array of shape N x E where N is number of vectors and E is their dimension
+
+    Returns
+    -------
+    normalized_vectors: np.ndarray
+        Same vectors but normalized 
+    """
+    norms = np.apply_along_axis(np.linalg.norm, arr = vectors_array, axis = 1) # Shape N 
+    normalized_vectors = vectors_array/norms[:, np.newaxis]
+    return normalized_vectors
 
 "--------------------------------------------------------------------------------"
