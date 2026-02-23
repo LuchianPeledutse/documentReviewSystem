85c2c43f80afac55fdaa927398d06f6f8406a5d4
Luchian <luchian-1999@mail.ru>
2026-02-23 08:07:14 +0300

    Loads chunks in V_DB and R_DB, but the number of chunks differ. Needs testing

--- dataScripts/vector_database_fill.py
+++ dataScripts/vector_database_fill.py
@@ -7,19 +7,23 @@ sys.path.extend(["..\\", "..\\backend\\"])
 import pypdf
 import faiss
 
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 from tqdm import tqdm
 
 from data_utils import TextEmbedding, PdfChunkReader
-from relational_database import insert_chunks
+from relational_database import Chunk
 
-PDF_DOCS_PATH = None
-VECTOR_DATABASE_PATH = None
+# Creating connection to database
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+engine = create_engine(f"postgresql://postgres:{DB_PASSWORD}@localhost/prototypedocumentreviewsystem")
 
 class FaissLoader:
     def __init__(self, embedding_class: TextEmbedding):
         self.embedding_class = embedding_class
 
-    def load_and_save_database(self, folder_path: str, save_to: str, db_chunk_add: Callable[[List[str], List[int]], None]) -> None:
+    def load_and_save_database(self, folder_path: str, save_to: str, engine) -> None:
         """
         Loads all documents from folder path that contains only pdf docs
         chunks them and embeds into vector database (embedding vectors are normalized)
@@ -28,18 +32,26 @@ class FaissLoader:
         """
         index = faiss.IndexFlatIP(self.embedding_class.model.embeddings.word_embeddings.weight.shape[1])
         pdf_filenames = os.listdir(folder_path)
+        SESSION = Session(engine)
         CURRENT_TOTAL_VECTORS = 0
         for pdf_filename in tqdm(pdf_filenames, desc="Going through filenames..."):
             chunks = PdfChunkReader(folder_path + "\\" + pdf_filename).get_chunks()
+            print(f"Length of chunks: {len(chunks)}")
+            print(f"Number of vectors in database: {index.ntotal}")
             embedded_chunks_yielding = self.embedding_class.embed(chunks)
-            previous_total = CURRENT_TOTAL_VECTORS
-            for chunk_batch in embedded_chunks_yielding:
+            previous_total = 0
+            for chunk_batch in embedded_chunks_yielding:    
                 index.add(chunk_batch)
-                next_total = index.ntotal
-                db_chunk_add(chunks[previous_total:next_total], [id for id in range(previous_total, next_total)])
+                next_total = index.ntotal - CURRENT_TOTAL_VECTORS
+                slice_chunks = chunks[previous_total:next_total]
+                clean_slice_chunks = [chunk.replace('\x00', '') for chunk in slice_chunks]
+                chunk_ids = list(range(previous_total + CURRENT_TOTAL_VECTORS, next_total + CURRENT_TOTAL_VECTORS))
+                db_chunks = [Chunk(id=id, chunk=chunk) for id, chunk in zip(chunk_ids, clean_slice_chunks)]
+                SESSION.add_all(db_chunks)
+                SESSION.commit()
                 previous_total = next_total
-            CURRENT_TOTAL_VECTORS = previous_total
-        
+            CURRENT_TOTAL_VECTORS = previous_total + CURRENT_TOTAL_VECTORS
+        SESSION.close()
         faiss.write_index(index, save_to)
         return None
 
@@ -48,4 +60,4 @@ if __name__ == "__main__":
     faiss_loader = FaissLoader(embedding_class=embedding_class)
     faiss_loader.load_and_save_database(folder_path= "C:\\main\\GitHub\\documentReviewSystem\\knowledge_data",
                                         save_to= "C:\\main\\GitHub\\documentReviewSystem\\project_data\\vector_db.index",
-                                        db_chunk_add=insert_chunks)
+                                        engine= engine)
