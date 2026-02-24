b5b1f79ac4d93d262542ed84a8f3bbfb94461627
Luchian <luchian-1999@mail.ru>
2026-02-24 04:38:36 +0300

    Implemented gigachat generation based on extracted chunks

--- backend/modelAPI.py
+++ backend/modelAPI.py
@@ -7,20 +7,54 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-# prompts
+# PROMPTS
+def summarize_technologies(file_input: str):
+    summarization = f"""
+    Task: Gven text below extract all the technologies, following the instructions\n
+    Text: {file_input}\n
+    INSTRUCTION: Give an answer of the form that is similar to example
+    EXAMPLE: LLM and their usage in the modern world. But how do you actually write code
+    that is similar to code in production? Using Git, of course. The models are usually trained using
+    the optimizers: like Adam, SGD and many other 
+    
+    LLM (their usage in the modern world) Git (code in production) Optimizers (Adam, SGD for training models)
 
+    WHAT NOT TO DO: Write any additional text that is not related to technologies extracted  
+"""
+    return summarization
+
+def suggest_additions(tech: str, additional_text: str) -> str:
+    augmented_with_chunks = f"""
+    Given the technologies and additional information, write suggestions of technologies to add base on information\n
+    TECHNOLOGIES: {tech}\n
+    INFORMATION: {additional_text}\n
+    WHAT NOT TO DO:
+    Write something besides the technologies to add
+"""
+    return augmented_with_chunks
 # TOKENS
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
-app = FastAPI()
+# Functions
+def get_access_token(auth_token):
+  url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 
-class TokenRequest(BaseModel):
-    text: str
+  payload = 'scope=GIGACHAT_API_PERS'
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json',
+    'RqUID': f'{str(uuid.uuid4())}',
+    'Authorization': f'Basic {auth_token}'
+  }
 
-class TokenResponse(BaseModel):
-    token: str
+  response = requests.request("POST", url, headers=headers, data=payload, verify=False)
 
-def generate_tokens(prompt: str, access_token: str) -> str:
+  ACCCESS = response.text
+  ACCCESS_TOKEN = json.loads(ACCCESS)["access_token"]
+  return ACCCESS_TOKEN
+
+
+def generate_tokens(prompt: str, access_token: str = get_access_token(AUTH_TOKEN)) -> str:
     rq_uid = str(uuid.uuid4())
     url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
     payload = json.dumps({
@@ -49,10 +83,25 @@ def generate_tokens(prompt: str, access_token: str) -> str:
     response = requests.request("POST", url, headers=headers, data=payload, verify=False)
     return json.loads(response.text)["choices"][0]["message"]["content"]
 
+def get_similar_db_chunks(content: str) -> str:
+    pass
+
+# API class
+app = FastAPI()
+
+class TokenRequest(BaseModel):
+    text: str
+
+class TokenResponse(BaseModel):
+    tokens: str
+
 @app.post("/generate-tokens", response_model = TokenResponse)
 def create_token(request: TokenRequest):
-    answer = generate_tokens(request.text)
-    return TokenResponse(token=answer)
+    technology_summary = generate_tokens(summarize_technologies(request.text))
+    augmentation = requests.request("POST", "http://localhost:8001/similar_chunks", json={"text": technology_summary})
+    augmented_with_info = suggest_additions(technology_summary, augmentation)
+    suggestion_answer = generate_tokens(augmented_with_info)
+    return TokenResponse(tokens=suggestion_answer)
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
