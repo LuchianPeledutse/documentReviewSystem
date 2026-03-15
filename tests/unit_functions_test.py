ba20340bf58176535cc5d5b433b62a20c4160f85
Luchian <luchian-1999@mail.ru>
2026-03-15 19:02:09 +0300

    Checked pdf chunking function

--- tests/unit_functions_test.py
+++ tests/unit_functions_test.py
@@ -1,18 +1,24 @@
+import os
+
 import sys
+import pypdf
 import random
 sys.path.append("C:\\main\\GitHub\\documentReviewSystem")
 
-from data_utils import normalize_vectors
-
+from data_utils import normalize_vectors, chunk_pdf_file_pages
 import pytest
 import numpy as np
 
-random_seed = 42
-rng = np.random.default_rng(random_seed)
+# GLOBAL VARIABLES
+TEST_PDF_FOLDER_PATH = "C:\\main\\GitHub\\documentReviewSystem\\data_test_pdf_chunks"
 
-# Vector normalization test
+RANDOM_SEED = 42
+rng = np.random.default_rng(RANDOM_SEED)
+
+# VECTOR NORMALIZATION TEST
 NUM_TESTS = 5_000
-@pytest.mark.parametrize("vector_matrix", [rng.random((10, random.randint(1, 101))) for _ in range(NUM_TESTS)])
+@pytest.mark.parametrize("vector_matrix",
+                         [rng.random((10, random.randint(1, 101))) for _ in range(NUM_TESTS)])
 def test_vector_normalization(vector_matrix, epsilon = 1e-10) -> np.array:
     """Test whether vectors ara properly normalized with normalize function"""
     normalized_matrix = normalize_vectors(vector_matrix)
@@ -24,4 +30,34 @@ def test_vector_normalization(vector_matrix, epsilon = 1e-10) -> np.array:
         norm_single_vector = single_vector/norm
         zero_matrix[row, :] = norm_single_vector
     assert bool((abs(normalized_matrix - zero_matrix) < epsilon).all())
+
+# PDF CHUNKER TEST
+NUM_TESTS = 10
+
+@pytest.fixture()
+def pdf_files_names_list():
+    pdf_files_names = os.listdir(TEST_PDF_FOLDER_PATH)
+    return pdf_files_names
+
+@pytest.mark.parametrize("chunk_length", 
+                         rng.integers(low = 50, high = 2000, size = NUM_TESTS).tolist())
+def test_pdf_chunker(pdf_files_names_list, chunk_length: int) -> None:
+    "This function checks pdf chunker function from data_utils file"
+    for pdf_name in pdf_files_names_list:
+        full_pdf_path = TEST_PDF_FOLDER_PATH + f"\\{pdf_name}"
+        # Extract text from each pdf page and compare that text to joined chunks
+        pdf_reader = pypdf.PdfReader(full_pdf_path)
+        pdf_pages_chunks = chunk_pdf_file_pages(pdf_path = full_pdf_path,
+                                                chunk_length = chunk_length)
+        # Iterate through pages and compare their text to joined chunk text
+        current_page_idx = 0
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            assert page_text == "".join(pdf_pages_chunks[current_page_idx]), f"Error on page {current_page_idx}"
+            current_page_idx += 1
+
+
+
+def test_r():
+    assert "sdkfjsf\njsdj\nwhat" == "sdk324\njsdj\nwhat"
 '--------------------------------------------------------------------------------'
