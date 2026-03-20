58bc36b18e25ad6fff5e9a11a23b87b84c038455
Luchian <luchian-1999@mail.ru>
2026-03-20 21:52:33 +0300

    Implemented vector database

--- systemComponents.py
+++ systemComponents.py
@@ -1,14 +1,20 @@
 import os
 import uuid
 import json
-from typing import List, Tuple, Callable
+from typing import List, Tuple, Dict, Callable
+
+import requests
+from tqdm import tqdm
 
 from data_utils import normalize_vectors
 
-import requests
-
+import faiss
 import torch
 import numpy as np
+
+from sqlalchemy import Text, Integer
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column
 
 from transformers import AutoModel, AutoTokenizer
 
@@ -31,10 +37,6 @@ def embedding_model(docs: List[str], device: str = 'cuda') -> np.ndarray:
 
     normalized_embeddings = normalize_vectors(query_embeddings.detach().cpu().numpy())
     return normalized_embeddings
-
-    
-
-'--------------------------------------------------------------------------------'
 
 class LlmModelModule:
     def __init__(self, access_token: str):
@@ -76,23 +78,67 @@ class VectorDb:
     """
     Implementation of vector database (vector index with additional operations)
     """
-    def __init__(self):
+    def __init__(self, relational_db_engine: Engine, tablename: str, 
+                 embedding_function: Callable[[List[str]], np.ndarray]):
         self.vector_index = None
+        self.embedding_function = embedding_function
+        self.relational_db_engine = relational_db_engine
+        # Create table with specific name
+        class Base(DeclarativeBase):
+            pass
+        class Chunk(Base):
+            __tablename__ = tablename
 
-    def load_database(self, load_path: str) -> None:
+            id: Mapped[int] = mapped_column(primary_key = True)
+            content: Mapped[str]
+            label: Mapped[int]
+        # Create table in postgres database and save it as a model
+        Base.metadata.create_all(relational_db_engine)
+        self.Chunk = Chunk
+
+    def create_index(self, dim: int = 1024) -> None:
+        """
+        Builds inner product index given the number of dimensions
+        """
+        self.vector_index = faiss.IndexFlatIP(dim)
+
+    def load_index(self, load_path: str) -> None:
         "Loads database given path"
-        pass
+        self.vector_index = faiss.read_index(load_path)
+        print(f"Vector index read from the path {load_path}")
 
-    def save_database(self, save_path: str) -> None:
+    def save_index(self, save_path: str) -> None:
         "Saves database given path"
-        pass
+        faiss.write_index(self.vector_index, save_path)
+        print(f"Vector index saved at {save_path}")
 
+    def load_chunks(self, chunks: List[str], label: int, batch_size: int = 3) -> None:
+        """
+        Embeds chunks and loads them to faiss vector database
+        Chunks, their label, and corresponding ids are saved to relational database
+        """
+        assert self.vector_index is not None, "Vector index has to be initialized"
+        N_total = len(chunks)
+        VI_total = self.vector_index.ntotal
+        # Load embedded chunks to vector index
+        for i in tqdm(range((N_total//batch_size + 1) if N_total%batch_size != 0 else N_total//batch_size),
+                      desc=f"Loading chunks to database with batch size {batch_size}"):
+            embedded_batch = self.embedding_function(chunks[i*batch_size: ((i+1)*batch_size if (i+1)*batch_size <= N_total else N_total)])
+            self.vector_index.add(embedded_batch)
+        # load chunks with their corresponding ids and labels
+        with Session(self.relational_db_engine) as session:
+            chunk_models = [
+                self.Chunk(id=VI_total + chunk_idx, content=chunks[chunk_idx], label=label)
+                for chunk_idx in range(N_total)
+            ]
+            session.add_all(chunk_models)
+            session.commit()
+    
 class RetrieverModule:
     def __init__(self, embedding_model: Callable[[List[str]], np.ndarray],
-                 vector_database: VectorDb, relational_db_engine: str):
+                 vector_database: VectorDb):
         self.embedding_model = embedding_model
         self.vector_database = vector_database
-        self.relational_db_engine = relational_db_engine
 
     def retrieve(self, text: str) -> List[Tuple[int, str]]:
         """
