43f4672366858b7505e286637078ef25623213ac
Luchian <luchian-1999@mail.ru>
2026-03-25 01:13:51 +0300

    Implemented retriever module

--- systemComponents.py
+++ systemComponents.py
@@ -1,7 +1,11 @@
 import os
+import re
 import uuid
 import json
+import warnings
 from typing import List, Tuple, Dict, Callable
+
+warnings.filterwarnings("ignore")
 
 import requests
 from tqdm import tqdm
@@ -12,30 +16,31 @@ import faiss
 import torch
 import numpy as np
 
-from sqlalchemy import Text, Integer
+from sqlalchemy import Text, Integer, select
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column
 
 from transformers import AutoModel, AutoTokenizer
 
 EMBEDDING_MODEL_NAME = 'Snowflake/snowflake-arctic-embed-l-v2.0'
+MODEL = AutoModel.from_pretrained(EMBEDDING_MODEL_NAME, add_pooling_layer = False).to(device = 'cuda')
+TOKENIZER = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_NAME)
 
 def embedding_model(docs: List[str], device: str = 'cuda') -> np.ndarray:
     """
     Given a list of documents returns their respective normalized embeddings
     Model used is hardcoded. In future might be softened with ONNX
     """
-    tokenizer = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_NAME)
-    model = AutoModel.from_pretrained(EMBEDDING_MODEL_NAME,
-                                      add_pooling_layer = False).to(device = device)
+    tokenizer = TOKENIZER
+    model = MODEL
     model.eval()
-    tokenized_queries = tokenizer(["query: " + doc for doc in docs],
+    tokenized_queries = tokenizer(docs,
                                   padding=True, truncation=True,
                                   return_tensors='pt', max_length=8192).to(device = 'cuda')
     with torch.no_grad():
         query_embeddings = model(**tokenized_queries)[0][:, 0]
 
-    normalized_embeddings = normalize_vectors(query_embeddings.detach().cpu().numpy())
+    normalized_embeddings = normalize_vectors(query_embeddings.contiguous().detach().cpu().numpy())
     return normalized_embeddings
 
 class LlmModelModule:
@@ -118,7 +123,13 @@ class VectorDb:
         Chunks, their label, and corresponding ids are saved to relational database
         """
         assert self.vector_index is not None, "Vector index has to be initialized"
+        chunks_without_null = filter(lambda item: not bool(re.findall(r"\x00", item)), chunks)
+        chunks = list(map(lambda x: x.encode('utf-8', errors='ignore').decode('utf-8'), chunks_without_null))
         N_total = len(chunks)
+        print(f"Length of chunks: {N_total}")
+        print("".join(["-" for  _ in range(1000)]))
+        print(chunks)
+        print("".join(["-" for  _ in range(1000)]))
         VI_total = self.vector_index.ntotal
         # Load embedded chunks to vector index
         for i in tqdm(range((N_total//batch_size + 1) if N_total%batch_size != 0 else N_total//batch_size),
@@ -133,19 +144,34 @@ class VectorDb:
             ]
             session.add_all(chunk_models)
             session.commit()
+        
+        
+        
     
 class RetrieverModule:
-    def __init__(self, embedding_model: Callable[[List[str]], np.ndarray],
-                 vector_database: VectorDb):
-        self.embedding_model = embedding_model
+    def __init__(self, vector_database: VectorDb, k: int = 10):
         self.vector_database = vector_database
+        self.k = k
+    
+    @property
+    def vector_index(self):
+        return self.vector_database.vector_index
 
     def retrieve(self, text: str) -> List[Tuple[int, str]]:
         """
         Given text retrieves the indicies from faiss database and chunks from postgres database
         Each index relates to its chunk, respectively
         """ 
-        pass
+        vector_text = self.vector_database.embedding_function(["query: " + text],
+                                                              device = "cpu")
+        search_result = self.vector_index.search(vector_text, self.k)
+        closest_rows = []
+        for idx in search_result[1].flatten().tolist():
+            statement = select(self.vector_database.Chunk).where(self.vector_database.Chunk.id == idx)
+            with Session(self.vector_database.relational_db_engine) as session:
+                selected_row = session.execute(statement).all()
+                closest_rows.append((selected_row[0][0].id, selected_row[0][0].content, selected_row[0][0].label))
+        return closest_rows
 
 class AugmentationModule:
     def __init__(self, prompt: str):
