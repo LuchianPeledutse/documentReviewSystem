4bf11bc6c05f36fdf26e6af754b5b6403fa02177
Luchian <luchian-1999@mail.ru>
2026-04-18 03:46:38 +0300

    Connected model and chunk api to frontend

--- frontend/streamlit_demo.py
+++ frontend/streamlit_demo.py
@@ -1,45 +1,64 @@
+import os
+import sys
+from io import BytesIO
+sys.path.append("..\\")
+
 from typing import List
+from pypdf import PdfReader
+from data_utils import extract_pages_from_pdf
 
 import requests
 import streamlit as st
 
-BACKEND_URL = "http://localhost:8000/process"  # Replace with actual backend URL
-
-# def extract_text_from_pdf(pdf_file) -> List[str]:
-#     """Extract text from each page of PDF, return list of page texts."""
-#     reader = PyPDF2.PdfReader(pdf_file)
-#     return [page.extract_text() or "" for page in reader.pages]
+LLM_URL = os.getenv("LLM_URL")
+BACKEND_URL = os.getenv("BACKEND_URL")
+NER_model_URL = os.getenv("NER_model_URL")
 
 st.set_page_config(layout="wide")
-st.title("Document Review System")
+st.title("Система аудита учебных материалов")
 
 left_col, right_col = st.columns([2, 1])
 
 with left_col:
-    uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+    uploaded_file = st.file_uploader("Загрузить pdf", type="pdf")
     if uploaded_file is not None:
-        st.success("PDF loaded")
+        st.success("Файл загружен")
         # Optionally display PDF preview (basic)
-        st.markdown(f"**File:** {uploaded_file.name}")
-
-        if st.button("Process Document"):
-            with st.spinner("Processing ..."):
-                pages_text = ["one", "two", "three"] # extract_text_from_pdf(uploaded_file)
-                payload = {"pages": pages_text}
-                try:
-                    # response = requests.post(BACKEND_URL, json=payload)
-                    # response.raise_for_status()
-                    # result = response.json()
-                    result = {"key technologies": "ML", "key technologies from knowledge base documents": "Semantic technologies", "suggested recommendations": "Something from gigachat model"}
-                except Exception as e:
-                    st.error(f"Backend error: {e}")
-                    st.stop()
+        st.markdown(f"**PDF файл:** {uploaded_file.name}")
+        if st.button("Обработать документ"):
+            with st.spinner("Обработка документа ..."):
+                    page_texts = extract_pages_from_pdf(BytesIO(uploaded_file.getvalue()))
+                    try:
+                        # Collect technologies extracted from the documents provided
+                        extracted_doc_technologies = []
+                        for page_text in page_texts:
+                             # Here we employ the NER model
+                             extracted_doc_technologies.append("ML")
+                        retrieved_model = requests.post(url = "http://127.0.0.1:8001/chunks", 
+                                                        json = {"text": "NLP, ML, LLMs, retriever, matrix operations, Spline methods"})
+                        retrieved_chunks = retrieved_model.json()["chunks"]
+                        # Collect technologies extracted from the chunks provided
+                        extracted_knowledge_base_technologies = []
+                        for chunk in retrieved_chunks:
+                            # Here we employ NER model
+                            extracted_knowledge_base_technologies.append("LLM")
+                        all_technologies = extracted_doc_technologies + extracted_knowledge_base_technologies
+                        llm_suggestions = requests.post("http://127.0.0.1:8000/suggestions",
+                                                        json = {"string_technologies": " ".join(all_technologies)})
+                        result = {"document_technologies": " ".join(extracted_doc_technologies),
+                                  "knowledge_base_technologies": " ".join(extracted_knowledge_base_technologies),
+                                  "model_recommendations": llm_suggestions.json()["answer"]}
+                    except Exception as e:
+                        st.error(f"Backend error: {e}")
+                        st.stop()
 
             with right_col:
-                st.subheader("Results")
-                st.markdown("**Key Technologies**")
-                st.write(result.get("key technologies", "N/A"))
-                st.markdown("**Key Technologies from Knowledge Base**")
-                st.write(result.get("key technologies from knowledge base documents", "N/A"))
-                st.markdown("**Suggested Recommendations**")
-                st.write(result.get("suggested recommendations", "N/A"))
+                st.subheader("Результаты")
+                st.markdown("**Технологии извлеченные из документа**")
+                st.write(result.get("document_technologies", "N/A"))
+                st.markdown("**Технологии извлеченные из внутренних документов**")
+                st.write(result.get("knowledge_base_technologies", "N/A"))
+                st.markdown("**Рекомандации на основе параметрических знаний модели**")
+                st.write(result.get("model_recommendations", "N/A"))
+
+"--------------------------------------------------------------------------------"
